e9c3bfcea4739be670a7525b67a6c5b8044a1d00
emilyalfs <46984335+emilyalfs@users.noreply.github.com>
2025-12-03 17:50:08 -0600

    clean up

--- phase_1.sh
+++ phase_1.sh
@@ -10,14 +10,21 @@ if [ ! -d "./${project_name}" ]; then
   echo "Directory '$project_name' setup."
 else
   echo "Directory '$project_name' already exists and files will be overwritten"
+  if [ ! -d "./${project_name}/results/" ]; then
+  	mkdir -p "./${project_name}/results/"
+  fi
+  if [ ! -d "./${project_name}/out_files/" ]; then
+  	mkdir -p "./${project_name}/out_files/"
+  fi
+  if [ ! -d "./${project_name}/first_frames/" ]; then
+  	mkdir -p "./${project_name}/first_frames/"
+  fi  
 fi
 
 for file in $pth*.mp4; do
-		if [[ "$file" == *"$isolate"* ]]; then
-			prefix=${file##*/}
-                        prefix=${prefix::-4}
-                        #echo "$file" "$prefix" "${step}_runner.sh"
-			sbatch --job-name=$prefix-1 --output=./$project_name/out_files/$prefix-1.out 1_runner.sh $file $project_name
-                        sbatch --job-name=$prefix-2 --output=./$project_name/out_files/$prefix-2.out 2_runner.sh $file $project_name
-                fi
+	prefix=${file##*/}
+	prefix=${prefix::-4}
+	#echo "$file" "$prefix" "${step}_runner.sh"
+	sbatch --job-name=$prefix-1 --output=./$project_name/out_files/$prefix-1.out 1_runner.sh $file $project_name
+	sbatch --job-name=$prefix-2 --output=./$project_name/out_files/$prefix-2.out 2_runner.sh $file $project_name
 done
