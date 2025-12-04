e0c04dc1e629a05f76c6d6c4529bd2d38cc323dd
emilyalfs <46984335+emilyalfs@users.noreply.github.com>
2025-12-03 18:01:51 -0600

    Update extract_first_frames.py

--- extract_first_frames.py
+++ extract_first_frames.py
@@ -1,19 +1,19 @@
 import os
 import cv2
 import shutil
+import sys
 
-paf = "/homes/bhavana/rat-scoring/batch/videos/NO_TMS/"
-project_name = "projectX"
+paf = sys.argv[1]
+project_name = sys.argv[2]
 
 files = os.listdir(paf)
 files = [i for i in files if ".mp4" in i]
 
 for vid in files:
-    print(vid)
     cap = cv2.VideoCapture(paf+vid)
     _, frame = cap.read()
     short = vid[:-4]
     cap.release()
     cv2.imwrite(f"./{project_name}/first_frames/{short}.png",frame)
 shutil.make_archive("./{project_name}/first_frames",'zip',f"./{project_name}/first_frames")
-
+print(f"Extracted first frames from {len(files)} videos") 
